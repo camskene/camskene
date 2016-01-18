d5c4e30979f9c0736b794cf05e8657d15a60a5d7
Cam Skene <camskene@gmail.com>
2016-01-18 01:09:13 -0500

    update grunt build task, update image paths

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,52 @@
 /* global module */
+
+/*
+ $ npm install grunt-contrib-uglify grunt-autoprefixer grunt-contrib-sass grunt-contrib-watch grunt-contrib-concat grunt-contrib-jshint grunt-contrib-clean grunt-notify grunt-contrib-connect grunt-contrib-copy grunt-contrib-cssmin grunt-browserify grunt-filerev grunt-userev2 --save-dev
+*/
+
 module.exports = function(grunt) {
+
   grunt.initConfig({
+
+    // Server
+    connect: {
+      server: {
+        options: {
+          port: 1337,
+          base: './dist',
+          keepalive: true,
+          livereload: true
+        }
+      }
+    },
+
+    // copy html
+    copy: {
+      html: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['*.html'],
+          dest: 'dist/'
+        }]
+      },
+      images: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['images/**/*'],
+          dest: 'dist/'
+        }]
+      },
+      fonts: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['fonts/**/*'],
+          dest: 'dist/'
+        }]
+      }
+    },
 
     // Sass
     sass: {
@@ -46,14 +92,14 @@ module.exports = function(grunt) {
     // Concat
     concat: {
       js: {
-        src: ['src/scripts/plugins.js', 'src/scripts/main.js'],
+        src: ['src/scripts/plugins.js','src/scripts/main.js'],
         dest: 'dist/scripts/main.js'
       }
     },
 
     // Uglify
     uglify: {
-      dist: {
+      js: {
         src: 'dist/scripts/main.js',
         dest: 'dist/scripts/main.min.js'
       }
@@ -62,7 +108,7 @@ module.exports = function(grunt) {
 
     // Clean
     clean: {
-      build: ['dist/styles', 'dist/scripts']
+      build: ['dist/styles', 'dist/scripts', 'dist/images']
     },
 
     // Notify
@@ -83,21 +129,69 @@ module.exports = function(grunt) {
     watch: {
       styles: {
         files: 'src/styles/**/*.scss',
-        tasks: ['sass', 'autoprefixer', 'cssmin', 'notify:styles'],
+        tasks: ['sass', 'autoprefixer'],
       },
       scripts: {
         files: 'src/scripts/**/*.js',
-        tasks: ['concat', 'uglify', 'notify:scripts'],
+        tasks: ['concat'],
+      },
+      images: {
+        files: 'src/images/**/*',
+        tasks: ['copy:images'],
+      },
+      html: {
+        files: 'src/*.html',
+        tasks: ['copy:html'],
       },
       livereload: {
-        options: { livereload: true },
+        options: {
+          livereload: true
+        },
         files: [
-          './*.html',
+          'dist/*.html',
           'dist/styles/**/*.css',
           'dist/scripts/**/*.js',
           'dist/images/**/*'
         ]
       }
+    },
+
+    // File rev files
+    filerev: {
+      options: {
+        encoding: 'utf8',
+        algorithm: 'md5',
+        length: 8
+      },
+      sourcemaps: { // Rename sourcemaps.
+        src: ['dist/styles/*.map.css', 'dist/scripts/*.map.js'],
+      },
+      assets: { // Rename minified js/css.
+        src: ['dist/styles/*.css', 'dist/scripts/*.js', '!dist/styles/*.map.css', '!dist/scripts/*.map.js'],
+      },
+    },
+
+    userev: {
+      options: {
+        hash: /(\.[a-f0-9]{8})\.[a-z]+$/,
+      },
+      assets: { // Link to sourcemaps in minified js/css.
+        src: ['dist/styles/*.css', 'dist/js/*.js', '!dist/styles/*.map.css', '!dist/scripts/*.map.js'],
+        options: {
+          patterns: {
+            'Linking versioned source maps': /sourceMappingURL=([a-z0-9.]*\.map)/,
+          },
+        },
+      },
+      index: { // Link to minified js/css in index html.
+        src: 'dist/index.html',
+        options: {
+          patterns: {
+            'Linking versioned css assets': /(styles\/[a-z0-9./]*\.css)/g,
+            'Linking versioned js assets': /(scripts\/[a-z0-9./]*\.js)/g,
+          },
+        },
+      },
     }
   });
 
@@ -109,18 +203,45 @@ module.exports = function(grunt) {
     'uglify',
     'sass',
     'autoprefixer',
-    'cssmin'
+    'cssmin',
+    'copy',
+    'connect'
+  ]);
+
+  // Build task
+  grunt.registerTask('build', [
+    'jshint',
+    'clean',
+    'concat',
+    'uglify',
+    'sass',
+    'autoprefixer',
+    'cssmin',
+    'copy',
+    'rev'
   ]);
 
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-filerev');
+  grunt.loadNpmTasks('grunt-userev2');
+
+  grunt.registerTask('rev', [
+    'filerev:sourcemaps', // Rename sourcemaps.
+    'userev:assets', // Link to sourcemaps in minified js/css.
+    'filerev:assets', // Rename minified js/css.
+    'userev:index', // Link to minified js/css in index html.
+  ]);
 
 };
